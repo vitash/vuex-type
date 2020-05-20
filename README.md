a7ae8c21df312eed1d0cc181a41cf87815d60552
vitash <18176159774@163.com>
2020-05-20 23:02:56 +0800

    良辰吉日，月色真美，适合插猹

---
+++ README.md
@@ -0,0 +1,102 @@
+# vuex-type
+
+``` typescript
+import Vue from "vue"
+import Vuex, { Store, ModuleTree } from "vuex"
+import { useSStore, rootGetters, Getters, storeModule, State, rootModules, } from "vuex-type"
+
+declare module 'vuex-type/index' {
+    interface SStore<T> extends Store<T> {
+        getters: ExtractGetters<typeof getters>
+    }
+    interface StoreModules {
+        ss: ReturnType<typeof ss>
+        dd: ReturnType<typeof dd>
+    }
+}
+
+// in other files
+const ss = storeModule("ss", {
+    namespaced: true,
+    state: {
+        id: 0,
+        name: "",
+        flag: "A" as "A" | "B" | "C",
+        nums: [] as number[],
+    },
+    mutations: {
+        setName(state, data: { name: string, id: number }) {
+            state.id = 2
+            state.name = data.name
+        },
+        setNums(state, data: number[]) {
+            state.nums = data
+        }
+    },
+    actions: {
+        async setName(ctx, data: { name: string, id: number }) {
+            ctx.commit("setName", data)
+        },
+        async setNums({ commit, dispatch, state, rootGetters }, nums: number[]) {
+            commit("setNums", nums)
+            return { ao_li_gei: rootGetters.ao_li_gei, name: "ddd" }
+        }
+    }
+})
+
+// in other files
+const dd = storeModule("dd", {
+    namespaced: true,
+    state: {
+        count: 3
+    },
+    mutations: {
+        increment(state) {
+            state.count += 1
+        },
+        add(state, data: number) {
+            state.count += data
+        }
+    },
+    actions: {
+        async add({ commit }, nums: Promise<number[]>) {
+            let total = (await nums).reduce((acc, n) => acc += n)
+            commit("add", total)
+            // commit("ss/setName", { name: "ss", id: 0 }) // error, commit 到其他模块，目前无法推导
+            commit("ss/setName" as any, { name: "ss", id: 0 }) // any 
+        }
+    }
+})
+
+// in other files
+const getters = rootGetters({
+    ao_li_gei(state, g) {
+        return state.dd.count
+    },
+    name({ ss, dd }, g) {
+        return g.ao_li_gei + ss.name + dd.count
+    },
+})
+
+const modules = rootModules({}, ss, dd)
+
+export const store = new Vuex.Store({
+    getters,
+    modules,
+    plugins: [useSStore]
+})
+
+// vue components
+class VueTest extends Vue {
+    get s1() {
+        return this.$$store.state.ss.nums
+    }
+    fn1() {
+        this.$$store.commit(["ss", "setName"])({ name: "dd", id: 3 })
+
+        this.$$store.commit(["dd", "increment"])()
+        this.$$store.getters.ao_li_gei
+    }
+}
+
+```
---
+++ index.ts
@@ -0,0 +1,104 @@
+import Vue from 'vue'
+import { Store, MutationTree, GetterTree, ModuleTree, Commit, Dispatch } from 'vuex'
+
+export function useSStore(store: Store<State>) {
+    const dispatch_ = store.dispatch.bind(store)
+    store.dispatch = function (...arg: any[]) {
+        if (arg.length === 1 && Array.isArray(arg[0])) {
+            return (data: any) => dispatch_(arg[0].join('/'), data)
+        }
+        (dispatch_ as any)(...arg)
+    } as any
+
+    const commit_ = store.commit.bind(store)
+    store.commit = function (...arg: any[]) {
+        if (arg.length === 1 && Array.isArray(arg[0])) {
+            return (data: any) => commit_(arg[0].join('/'), data)
+        }
+        (commit_ as any)(...arg)
+    } as any
+
+    Vue.prototype.$$store = store
+}
+
+export function storeModule<
+    ModuleKey extends ModulesKeys,
+    S,
+    Mut extends MutationTree<S>,
+    Act extends ActTree<ModuleKey, S, State>,
+    >(key: ModuleKey, mod: {
+        namespaced: true
+        state: S,
+        mutations?: Mut,
+        actions?: Act,
+        // modules?: ModuleTree<any> /** 类型推导不出来，QAQ */
+    }) {
+    return (node: ModuleTree<any>) => {
+        node[key] = mod as any
+        return mod
+    }
+}
+
+// export type UseModule = (node: ModuleTree<any>) => any
+export function rootModules(root: ModuleTree<any>, ...mods: Array<(node: ModuleTree<any>) => any>) {
+    mods.forEach(m => m(root));
+    return root as StoreModules & ModuleTree<any>
+}
+
+// (state: State, getters: any) => any, 为什么第二个参数不使用 Getters 类型，因为会发生循环引用，求解 QAQ 。。。
+/** getters 不会对 Promise 解包，所以不要在 getters 里面写异步函数 */
+export function rootGetters<T extends Record<string, (state: State, getters: any) => any>>(getters: T) {
+    return getters
+}
+
+interface ActTree<Mkey extends ModulesKeys, S, State> {
+    // actions 必须是异步函数，如果不是请放到 mutations
+    [key: string]: (this: Store<State>, injectee: ActContext<Mkey, S, State>, payload?: any) => Promise<any>;
+}
+interface ActContext<Mkey extends ModulesKeys, S, R> {
+    dispatch: <FnName extends ModulesFnKeys<Mkey, 'actions'>>(type: FnName, ...payload: Parameters<ModulesFn<Mkey, 'actions', FnName>>) => ReturnType<ModulesFn<Mkey, 'actions', FnName>>;
+    commit: <FnName extends ModulesFnKeys<Mkey, 'mutations'>>(type: FnName, ...payload: Parameters<ModulesFn<Mkey, 'mutations', FnName>>) => void;
+    state: S;
+    getters: any;
+    rootState: R;
+    rootGetters: any; // Getters 会循环引用
+}
+
+type ModulesFnKeys<K1 extends ModulesKeys, K2 extends keyof StoreModules[K1]> = keyof StoreModules[K1][K2]
+
+type ModulesFn<
+    K1 extends ModulesKeys,
+    K2 extends keyof StoreModules[K1],
+    K3 extends keyof StoreModules[K1][K2]
+    > =
+    StoreModules[K1][K2][K3] extends (state: any, ...data: infer Data) => infer R ?
+    (...data: Data) => R : never
+
+export type ModulesKeys = keyof StoreModules
+export type RootState = { [K in ModulesKeys]: StoreModules[K]['state'] }
+export type State = ReadonlyDeep<RootState>
+export type Getters = SStore<State>['getters']
+
+export type ExtractGetters<G extends Record<keyof any, (...args: any) => any>> = { readonly [K in keyof G]: ReturnType<G[K]> }
+export type ReadonlyDeep<T> = { readonly [K in keyof T]: T[K] extends Object ? ReadonlyDeep<T[K]> : T[K] }
+// type PromiseUnwrap<T extends (...args: any) => any> = T extends (...arg: any[]) => Promise<infer U> ? U : ReturnType<T>
+
+export interface StoreModules { }
+export interface SStore<T> extends Store<T> { }
+
+declare module 'vue/types/vue' {
+    interface Vue {
+        $$store: SStore<State>;
+    }
+}
+
+declare module 'vuex/types/index' {
+    interface Dispatch {
+        <MKey extends ModulesKeys, FnName extends keyof StoreModules[MKey]['actions']>(names: [MKey, FnName]):
+            ModulesFn<MKey, 'actions', FnName>
+    }
+    interface Commit {
+        <MKey extends ModulesKeys, FnName extends keyof StoreModules[MKey]['mutations']>(names: [MKey, FnName]):
+            (...data: Parameters<ModulesFn<MKey, 'mutations', FnName>>) => void
+    }
+}
